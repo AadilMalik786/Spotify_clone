3c8738daa4104f84ad89ccdb6209f5985ac69ca1
Aadil malik <adilmalik786687@gmail.com>
2023-08-11 12:59:36 +0530

    scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -51,13 +51,13 @@ class App extends Component {
           </div>
         )}
 
-        <section className="ajju" style={{ height: "101vh", width: "100%", backgroundColor: "black" }}>
-          <div className="parent" style={{ display: "flex", justifyContent: "space-evenly"}}>
+        <section className="ajju" style={{ height: "100vh", width: "100%", backgroundColor: "black",overflow:"hidden" }}>
+          <div className="parent" style={{ display: "flex", justifyContent: "space-evenly",overflow:"hidden"}}>
             <div className="pappa" style={{ position: "relative", minWidth: "340px", height: "90vh", display: "flex", flexDirection: "column", alignItems: "center" ,gap:"10px",overflow:"hidden"}}>
               <Head></Head>
               <Head2></Head2>
             </div>
-            <article className="child" style={{ position: "relative", overflow: 'hidden', width: "300%", height: "90vh", display: "flex", flexDirection: "column", justifyContent: "space-evenly" }}>
+            <article className="child" style={{ position: "relative", overflow: 'hidden', width: "300%", height: "90vh", display: "flex", flexDirection: "column", justifyContent: "space-evenly",overflow:"hidden" }}>
 
               <Head5 handleLogin={this.handleLogin} />
               <CardManage handlePlaying={this.handlePlaying}></CardManage>
